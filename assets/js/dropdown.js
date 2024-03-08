ceff50371ce57dfef8018f48989d4edbca8a5153
SoroushGReza <mycodeschool22@outlook.com>
2024-03-08 18:51:32 +0000

    - Implemented: JavaScript function to toggle display diploma dropdown, when diploma icon is clicked. - Added: event listener to close the diploma dropdown if the click is outside the diploma icon.

---
+++ assets/js/dropdown.js
@@ -0,0 +1,25 @@
+// Code Institute Diploma dropdowm Image
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("diploma-icon").onclick = function (event) {
+    var dropdown = document.getElementById("diploma-dropdown");
+    if (dropdown.style.display === "none") {
+      dropdown.style.display = "block";
+    } else {
+      dropdown.style.display = "none";
+    }
+    event.stopPropagation();
+  };
+
+  window.onclick = function (event) {
+    if (!event.target.matches("#diploma-icon")) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.style.display === "block") {
+          openDropdown.style.display = "none";
+        }
+      }
+    }
+  };
+});

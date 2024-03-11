b529cf01c6e32d9ca4b5751f1a5c254e11cf4b1a
SoroushGReza <mycodeschool22@outlook.com>
2024-03-11 19:27:14 +0000

    Added: alert-success message when email is sent successfully (closes after 8 seconds) and styling to it in css.

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,19 +1,35 @@
-//     Send Email
+// ------------------------------ Send Email
 
 function sendEmail(contactForm) {
-  emailjs.send("outlook", "soroush", {
-      "from_name": contactForm.name.value,
-      "from_email": contactForm.emailaddress.value,
-      "project_request": contactForm.projectsummary.value,
-      "reply_to": contactForm.emailaddress.value,
+  emailjs
+    .send("outlook", "soroush", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
+      reply_to: contactForm.emailaddress.value,
     })
     .then(
       function (response) {
         console.log("SUCCESS", response);
+        //    Show alert message
+        var alertBox = document.querySelector(".alert");
+        alertBox.style.display = "block";
+        //    Reset form
+        contactForm.reset();
+        //    Close Alert after 8 seconds per auto
+        setTimeout(function () {
+          alertBox.style.display = "none";
+        }, 8000);
       },
       function (error) {
         console.log("FAILED", error);
       }
     );
-    return false;
+
+  return false;
+}
+
+//    Close alert message manually
+function closeAlert() {
+  document.querySelector(".alert").style.display = "none";
 }

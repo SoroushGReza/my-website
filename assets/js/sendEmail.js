4577fc18952f38be28bba200a24670a747582f8f
SoroushGReza <mycodeschool22@outlook.com>
2024-03-11 14:21:55 +0000

    Implemented: sendEmail function, and added script to contact.html

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,18 @@
+//     Send Email
+
+function sendEmail(contactForm) {
+  emailjs.send("outlook", "soroush", {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.emailaddress.value,
+      "project_request": contactForm.projectsummary.value,
+      "reply_to": contactForm.emailaddress.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+}

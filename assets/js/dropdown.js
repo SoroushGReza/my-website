f871b214584faa54300534e7abd04bd807e34540
SoroushGReza <mycodeschool22@outlook.com>
2024-03-15 18:23:39 +0000

    Added: Dropdowns for 'More Details' for every project in portfolio.html, and modified dropdown.js accordingly. Styling added through portfolio.css

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,4 +1,4 @@
-// Code Institute Diploma dropdowm Image
+//     --------  Code Institute Diploma dropdowm Image
 
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("diploma-icon").onclick = function (event) {
@@ -22,4 +22,40 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   };
+});
+
+//     ----------------- Project Details Drop-Down
+
+$(document).ready(function () {
+  // Toggle button and dropdown content based on the button's data-target attribute
+  $(".btn-details").click(function () {
+    var $this = $(this);
+    var targetSelector = $this.attr("data-target");
+    var $dropdownContent = $(targetSelector);
+
+    // Toggle visibility of the dropdown content and button
+    $dropdownContent
+      .collapse("toggle")
+      .on("shown.bs.collapse", function () {
+        $this.hide();
+      })
+      .on("hidden.bs.collapse", function () {
+        $this.show();
+      });
+  });
+
+  // Hide dropdown content when clicking outside of it
+  $(document).click(function (event) {
+    if (!$(event.target).closest(".btn-details, .collapse").length) {
+      // Iterate through each dropdown to hide and show the respective button
+      $(".collapse").each(function () {
+        var $this = $(this);
+        if ($this.hasClass("show")) {
+          $this.collapse("hide");
+          // Find the button that controls this dropdown and show it
+          $('button[data-target="#' + $this.attr("id") + '"]').show();
+        }
+      });
+    }
+  });
 });
